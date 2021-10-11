2559d87087806c518831e54077b48635b909d37b
Daren Wong <daren96123@gmail.com>
2021-10-12 07:33:10 +0800

    Split Topbar into smaller components and store in a folder

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -23,7 +23,7 @@ import FormGroup from "@material-ui/core/FormGroup";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import { Breadcrumbs } from "@material-ui/core";
-import DropdownMenu from "./DropdownMenu.js";
+import DropdownMenu from "../DropdownMenu.js";
 import { useAuth0 } from "@auth0/auth0-react";
 import { BiLogIn, BiLogOut } from "react-icons/bi";
 import SettingsIcon from "@material-ui/icons/Settings";
@@ -37,10 +37,11 @@ import HomeIcon from "@material-ui/icons/Home";
 import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
 import ExploreIcon from "@material-ui/icons/Explore";
 import ExploreOutlinedIcon from "@material-ui/icons/ExploreOutlined";
-import GIFVoteLogo from "../images/gif_vote_logo4.png";
-import PollForm from "./PollForm.js";
-import SignInPage from "./SignInPage.js";
-import { useSQL } from "../contexts/SQLContext";
+import GIFVoteLogo from "../../images/gif_vote_logo4.png";
+import PollForm from "../PollForm.js";
+import SignInPage from "../SignInPage.js";
+import MenuDrawer from "./MenuDrawer.jsx";
+import { useSQL } from "../../contexts/SQLContext";
 
 import { NavLink, useHistory, useLocation } from "react-router-dom";
 
@@ -178,7 +179,6 @@ export default function MenuAppBar() {
             src={GIFVoteLogo}
             onClick={() => history.push("/")}
           />
-          {false && <SimpleBreadcrumbs />}
           <div className={classes.topButtonContainer}>
             <div className={classes.topMainButtonContainer}>
               <IconButton
@@ -244,143 +244,3 @@ export default function MenuAppBar() {
   );
 }
 
-const SimpleBreadcrumbs = () => {
-  const classes = useStyles();
-  const { isAuthenticated } = useAuth0();
-
-  return (
-    <Breadcrumbs aria-label="breadcrumb" className={classes.breadcrumbs}>
-      <NavLink
-        className={classes.passiveLink}
-        activeClassName={classes.activeLink}
-        exact
-        to="/"
-      >
-        <WhatshotIcon className={classes.icon} />
-        Most Votes
-      </NavLink>
-      <NavLink
-        className={classes.passiveLink}
-        activeClassName={classes.activeLink}
-        to="/most-like"
-      >
-        <FavoriteBorderIcon className={classes.icon} />
-        Most Likes
-      </NavLink>
-      <NavLink
-        className={classes.passiveLink}
-        activeClassName={classes.activeLink}
-        to="/latest"
-      >
-        <ScheduleIcon className={classes.icon} />
-        Latest
-      </NavLink>
-      {isAuthenticated && (
-        <NavLink
-          className={classes.passiveLink}
-          activeClassName={classes.activeLink}
-          to="/your"
-        >
-          <FaceIcon className={classes.icon} />
-          Your Polls
-        </NavLink>
-      )}
-    </Breadcrumbs>
-  );
-};
-
-const MenuDrawer = () => {
-  const classes = useStyles();
-  const [isOpen, setIsOpen] = useState(false);
-  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
-  const { userId } = useSQL();
-
-  const history = useHistory();
-
-  return (
-    <>
-      <IconButton
-        edge="start"
-        className={classes.menuButton}
-        aria-label="menu"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer
-        anchor="left"
-        open={isOpen}
-        onClose={() => {
-          setIsOpen(false);
-        }}
-      >
-        <List>
-          <ListItem>
-            <ListItemAvatar>
-              {isAuthenticated && (
-                <Avatar
-                  src={user.picture}
-                  alt={user.name}
-                  onClick={() => {
-                    history.push(`/profile/${userId}`);
-                    setIsOpen(false);
-                  }}
-                />
-              )}
-              {!isAuthenticated && (
-                <Avatar alt={"Guest"}>
-                  <AccountCircle />
-                </Avatar>
-              )}
-            </ListItemAvatar>
-            <ListItemText
-              primary={user ? user.name : "Guest"}
-              secondary={user ? user.email : "Sign in to see your profile"}
-            />
-
-            {isAuthenticated && (
-              <IconButton className={classes.loginButton} onClick={logout}>
-                <BiLogOut />
-              </IconButton>
-            )}
-            {!isAuthenticated && (
-              <IconButton
-                className={classes.loginButton}
-                onClick={loginWithRedirect}
-              >
-                <BiLogIn />
-              </IconButton>
-            )}
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemIcon>
-              <FeedbackIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary={"Give Feedback"}
-              secondary={"Help us improve GIF Votes"}
-            />
-          </ListItem>
-          {false && (
-            <>
-              <Divider />
-              <ListItem button>
-                <ListItemIcon>
-                  <SettingsIcon />
-                </ListItemIcon>
-                <ListItemText primary={"Settings"} />
-              </ListItem>
-              <ListItem button>
-                <ListItemIcon>
-                  <InfoIcon />
-                </ListItemIcon>
-                <ListItemText primary={"About"} />
-              </ListItem>
-            </>
-          )}
-        </List>
-      </Drawer>
-    </>
-  );
-};
