b60bafbcf4b812a8be0c72d5fdc97da2bd16ae38
Daren Wong <daren96123@gmail.com>
2021-10-12 07:19:16 +0800

    removed unused components

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { ThemeProvider } from "@material-ui/styles";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import FixedListTest from "./FixedListTest";
-import FlexListTest from "./FlexListTest";
-import FlexListAPI from "./FlexListAPI";
 
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
